a4433956bf7fdaba25265f506035c747192cb880
esoft48 - Joao Vitor Ribeiro Assumpcao <jrvitor5@gmail.com>
2024-11-25 21:30:09 -0300

    feat: implement solution for remove duplicates

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCode.MergeSortedArray;
+using LeetCode.RemoveDuplicatedFromSortedArray;
 using LeetCode.RemoveElement;
 
 int[] array1 = new int[] { 1, 2, 3, 0, 0, 0 };
@@ -8,3 +9,6 @@ MergeSortedArray.Merge(array1, 3, array2, 3);
 int[] arrayRemoved = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
 var arraySize = RemoveElement.RemovesElement(arrayRemoved, 2);
 Console.WriteLine( arraySize);
+
+int[] removeDuplicated = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+var arrayDesduplicated = RemoveDuplicatesFromSortedArray.RemoveDuplicates(removeDuplicated);
---
+++ RemoveDuplicatedFromSortedArray/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.RemoveDuplicatedFromSortedArray
+{
+    internal class RemoveDuplicatesFromSortedArray
+    {
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            var count = 1;
+            var currIndex = 1;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1])
+                    continue;
+
+                nums[currIndex] = nums[i + 1];
+                currIndex++;
+            }
+
+            return currIndex;
+        }
+    }
+}
